491cd9d6662d201d87e94ccffe676a54a04379c7
Murilo Pereira <murilo@murilopereira.com>
2018-08-04 15:28:35 +0200

    HTTP server with actix-web.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
-extern crate hyper;
+use std::net::SocketAddr;
 
-#[macro_use]
 extern crate log;
 extern crate log4rs;
 
 #[macro_use]
 extern crate serde_json;
 
-use std::net::SocketAddr;
+extern crate actix_web;
+use actix_web::{http, server, App, Error, HttpRequest, HttpResponse};
 
-use hyper::rt::Future;
-use hyper::service::service_fn_ok;
-use hyper::{Body, Response, Server};
+fn json_body(content: &serde_json::Value) -> String {
+    format!("{}\n", serde_json::to_string_pretty(content).unwrap())
+}
+
+fn cluster_information(_request: &HttpRequest) -> Result<HttpResponse, Error> {
+    let cluster_information = json!({
+        "cluster_name" : "noronha",
+        "node_name" : "noronha-0",
+        "noronha_version" : "0.1.0"
+    });
+
+    Ok(HttpResponse::Ok()
+       .content_type("application/json")
+       .body(json_body(&cluster_information)))
+}
+
+fn http_api_application() -> App {
+    App::new().resource("/", |r| r.method(http::Method::GET).f(cluster_information))
+}
 
 fn main() {
     log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
@@ -20,26 +36,7 @@ fn main() {
     let port = 6500;
     let address: SocketAddr = format!("{}:{}", host, port).parse().unwrap();
 
-    let http_api = || {
-        service_fn_ok(|_| {
-            let data = json!({
-                "cluster_name" : "noronha",
-                "node_name" : "noronha-0",
-                "noronha_version" : "0.1.0"
-            });
+    let http_api = server::new(http_api_application).bind(address).unwrap();
 
-            let response_body = serde_json::to_string_pretty(&data).unwrap();
-
-            Response::new(Body::from(response_body))
-        })
-    };
-
-    let server = Server::bind(&address)
-        .serve(http_api)
-        .map_err(|e| error!("{}", e));
-
-    info!("listening at {}", address);
-    info!("started");
-
-    hyper::rt::run(server);
+    http_api.run();
 }
